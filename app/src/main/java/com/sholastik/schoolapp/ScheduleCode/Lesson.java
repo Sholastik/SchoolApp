1c9be39b61c6482a887cc852335f98e2a401482d
Vyacheslav <persikchicken@icloud.com>
2018-12-31 12:13:50 +0500

    Updated Lesson to match Room requirements

--- app/src/main/java/com/sholastik/schoolapp/ScheduleCode/Lesson.java
+++ app/src/main/java/com/sholastik/schoolapp/ScheduleCode/Lesson.java
@@ -1,43 +1,31 @@
 package com.sholastik.schoolapp.ScheduleCode;
 
 import android.arch.persistence.room.Entity;
-import android.arch.persistence.room.ForeignKey;
-import android.arch.persistence.room.PrimaryKey;
 import android.content.Context;
+import android.support.annotation.NonNull;
 
 import com.sholastik.schoolapp.R;
 
-import java.util.Calendar;
+import java.util.UUID;
 
-import static android.arch.persistence.room.ForeignKey.CASCADE;
-
-@Entity(tableName = "lessons", foreignKeys = @ForeignKey(entity = Day.class,
-        parentColumns = "mDayOfWeek",
-        childColumns = "mIndex",
-        onDelete = CASCADE))
+@Entity(tableName = "lessons", primaryKeys = {"mDayOfWeek", "mId"})
 public class Lesson {
-    @PrimaryKey(autoGenerate = true)
+    @NonNull
+    public String mId = UUID.randomUUID().toString();
+    public int mDayOfWeek;
     public int mIndex;
-    public int mId;
+
     public long mStartTime;
     public long mLength;
     public String mName;
-    public int mDayOfWeek;
 
     public Lesson(Context context, int dayOfWeek, int index) {
-        mName = context.getString(R.string.new_lesson);
         mDayOfWeek = dayOfWeek;
         mIndex = index;
 
-        Calendar startTimeCalendar = Calendar.getInstance();
-        startTimeCalendar.set(Calendar.HOUR, 8);
-        startTimeCalendar.set(Calendar.MINUTE, 15);
-        mStartTime = startTimeCalendar.getTime().getTime();
-
-        Calendar lengthCalendar = Calendar.getInstance();
-        lengthCalendar.set(Calendar.HOUR, 0);
-        lengthCalendar.set(Calendar.MINUTE, 45);
-        mLength = lengthCalendar.getTime().getTime();
+        mName = context.getString(R.string.new_lesson);
+        mStartTime = 0;
+        mLength = 0;
     }
 
     public Lesson() {
